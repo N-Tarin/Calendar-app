6b6f87535cc26d80f86d4ab340d09e47282d5d80
N-Tarin <karinuxxx1029@gmail.com>
2020-12-28 12:53:10 +0900

    add

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const today = new Date();
 const currentYear = today.getFullYear();
 const currentMonth = today.getMonth();
 
+
 const months = [
   'January',
   'February',
@@ -22,3 +23,9 @@ const months = [
 const month = document.getElementById('month');
 month.innerHTML = months[currentMonth];
 
+let days ="";
+
+for(let i = 1; i <= lastDays; i ++) {
+  days += `<th>${i}</th>`;
+  monthDays.innerHTML = days;
+}
