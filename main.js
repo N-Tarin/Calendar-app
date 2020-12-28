fd218607564837d8634e82bbe4dae54a71d0b65e
N-Tarin <karinuxxx1029@gmail.com>
2020-12-28 22:22:32 +0900

    add

--- main.js
+++ main.js
@@ -3,22 +3,24 @@
 const today = new Date();
 const currentYear = today.getFullYear();
 const currentMonth = today.getMonth();
-
+const currentToday = today.getDate();
 
 //月初めの日付を取得
-const startDate = new Date(currentYear, currentMonth , 1)
+const startDate = new Date(currentYear, currentMonth, 1) 
 const startDay = startDate.getDay()
 
 //月末の日付を取得
-const endDate = new Date(currentYear, currentMonth -1,  0) 
-const endDay = endDate.getDate() 
+const endDate = new Date(currentYear, currentMonth - 1,  0) 
+const endDay = endDate.getDate()
 
 //前月の日付を取得
-const lastMonthEndDate = new Date(currentYear, currentMonth,  0) 
-const lastMonthEndDay = lastMonthEndDate.getDate()
+const lastMonthEndDate = new Date(currentYear, currentMonth, 0) 
+const lastMonthEndDay = lastMonthEndDate.getDate() // 前月の末日
 
 //翌月の日付取得
-const nextMonthDate = new Date(currentYear, currentMonth, 0)
+const nextMonthStartDate = new Date(currentYear, currentMonth, 0) 
+const nextMonthStartDay = nextMonthStartDate.getDate()
+
 
 let dayCount = 1 // 日にちのカウント
 let calendarHtml = '' // HTMLを組み立てる変数
@@ -70,18 +72,19 @@ for (let w = 0; w < 6; w++) {
 
     for (let d = 0; d < 7; d++) {
         if (w == 0 && d < startDay) {
-          // 1行目で1日の曜日の前
-          let num = lastMonthEndDay - startDay + d + 1
-          calendarHtml += '<td class="tbody__day -prev">' + num + '</td>'
+            // 1行目で1日の曜日の前
+            let num = lastMonthEndDay - startDay + d + 1
+            calendarHtml += '<td class="tbody__day -prev">' + num + '</td>'
 
-        } else if (dayCount > endDay) {
-          // 末尾の日数を超えた
-          let num = dayCount - endDay
-          calendarHtml += '<td class="tbody__day -next">' + num + '</td>'
+          } else if (dayCount > endDay) {
+            // 末尾の日数を超えた
+            let num = dayCount - endDay
+            calendarHtml += '<td class="tbody__day -next">' + num + '</td>'
+            dayCount++
 
         } else {
-          calendarHtml += '<td class="tbody__day">' + dayCount + '</td>'
-          dayCount++
+            calendarHtml += '<td class="tbody__day">' + dayCount + '</td>'
+            dayCount++
         }
     }
     calendarHtml += '</tr>'
@@ -89,5 +92,7 @@ for (let w = 0; w < 6; w++) {
 calendarHtml += '</tbody>';
 calendarHtml += '</table>';
 
+
 const calendar = document.querySelector('.calendar')
 calendar.innerHTML = calendarHtml;
+
