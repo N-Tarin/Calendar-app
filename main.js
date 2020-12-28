58b032dbb47bfa5aef109c49c47eee1c5c78b9df
N-Tarin <karinuxxx1029@gmail.com>
2020-12-28 14:18:24 +0900

    add

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 'use strict';
 
+//年月を取得
 const today = new Date();
 const currentYear = today.getFullYear();
 const currentMonth = today.getMonth();
 
-
+//文字に変換
 const months = [
   'January',
   'February',
@@ -19,13 +20,31 @@ const months = [
   'November',
   'December',
 ];
-
 const month = document.getElementById('month');
 month.innerHTML = months[currentMonth];
 
-let days ="";
+//月末を取得
+const last = new Date(currentYear, currentMonth + 1, 0);
+const last_year  = last.getFullYear();
+const last_month = last.getMonth();
+const last_day   = last.getDate();
 
-for(let i = 1; i <= lastDays; i ++) {
-  days += `<th>${i}</th>`;
-  monthDays.innerHTML = days;
-}
+//カレンダーに出力
+for (let i=1; i<=last_day; i++) {
+  const week = new Date(last_year, last_month, i).getDay();
+  const tbody = document.querySelector('.tbody');
+  const row = document.createElement('tr');
+  const cell = document.createElement('td');
+
+  if (!week || i == 1) {
+    tbody.appendChild();
+      row.className = ".tbody__days";
+      cell.className = ".tbody__day";
+  
+      cell.innerHTML = (i);
+  }
+};
+
+
+
+
